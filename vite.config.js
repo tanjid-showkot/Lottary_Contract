a7d6f1748f2045fcd1144c208ae928e85d0e2d9d
Tanjid-showkot <tanjidsaykot611@gmail.com>
2024-01-28 00:36:13 +0600

    initialiging_git_pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lottery/',
-  plugins: [react()],
+  base: '/Lottary_Contract/',
+
+  plugins: [nodePolyfills(), react()],
 })
