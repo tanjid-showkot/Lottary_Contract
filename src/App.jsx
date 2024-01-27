88024f1aad20e67483cb3eac469fa7ec3c04b3ec
Tanjid-showkot <tanjidsaykot611@gmail.com>
2024-01-28 00:45:20 +0600

    initialiging_git_pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Home from './home';
+import Home from './Home';
 import {Toaster} from 'react-hot-toast'
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+// import { nodePolyfills } from 'vite-plugin-node-polyfills'
+// nodePolyfills(),
 
 import react from '@vitejs/plugin-react-swc'
 
@@ -7,5 +8,5 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/Lottary_Contract',
 
-  plugins: [nodePolyfills(), react()],
+  plugins: [react()],
 })
