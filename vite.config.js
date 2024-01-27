a138eaeda2e44483e529c65e7aba4a5c3df13202
Tanjid-showkot <tanjidsaykot611@gmail.com>
2024-01-28 01:04:49 +0600

    initialiging_git_pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Lottary_Contract',
+  base: '/Lottary_Contract/',
 
   plugins: [react()],
 })
